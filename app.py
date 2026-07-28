e57bf00cf5dadc484ce99dc302a1d68db856eecf
Miguel Santos <miguel.santos@agrorobotica.com.br>
2026-07-28 11:39:05 -0300

    att app

--- app.py
+++ app.py
@@ -271,12 +271,10 @@ if df_bruto.empty:
 df_fazendas = pd.read_excel("fazendas.xlsx")
 df_fazendas["Nome_Fazenda"] = df_fazendas["Nome_Fazenda"].str.strip()
 df_fazendas = df_fazendas.drop_duplicates(subset=["Cod_Fazenda", "Nome_Fazenda"])
-df_datas = pd.read_excel("datas_remessas.xlsx", dtype={'Remessa': str})
 
 df_bruto = (
     df_bruto
-    .merge(df_fazendas, how='inner', left_on='Fazenda', right_on='Cod_Fazenda')
-    #.merge(df_datas, how='inner', left_on=['Remessa', 'Unidade', 'Tipo'], right_on=['Remessa', 'Unidade', 'Tipo'])
+    .merge(df_fazendas, how='inner', left_on='Fazenda', right_on='Cod_Fazenda', validate='many_to_one')
 )
 df_bruto['Nome_Fazenda'] = df_bruto['Nome_Fazenda'].str.strip()
 
